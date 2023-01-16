72246aa71460822b9a0ec4b5c6caa2c7288db452
Kevin Washington <87237986+KevinWashington@users.noreply.github.com>
2023-01-16 17:51:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 let navigation = document.querySelector('nav')
+let whatsappFlutuante = document.querySelector(".whatsapp-flutuante");
 
 function onScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
+    whatsappFlutuante.classList.remove('d-none')
   } else {
     navigation.classList.remove('scroll')
+    whatsappFlutuante.classList.add("d-none");
+  }
+  if (scrollY > 300) {
+    whatsappFlutuante.classList.remove("d-none");
+  } else {
+    whatsappFlutuante.classList.add("d-none");
   }
 }
 
@@ -47,3 +55,23 @@ var swiper = new Swiper(".slide-content", {
       },
   },
 });
+
+function openmodal1() {
+  let modal = document.querySelector("#modal1");
+  modal.classList.remove("d-none")
+}
+
+function openmodal2() {
+  let modal = document.querySelector("#modal2");
+  modal.classList.remove("d-none");
+}
+
+function closemodal(){
+  let modal = document.querySelectorAll(".modals");
+  console.log(modal)
+  modal.forEach(element => {
+    if (!element.classList.contains("d-none")) {
+      element.classList.add("d-none")
+    }
+  });
+}
